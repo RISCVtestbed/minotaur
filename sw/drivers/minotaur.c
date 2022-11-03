e724f11b464639d2c6e09509b7a9be53a1fe202a
Nick Brown <n.brown@epcc.ed.ac.uk>
2022-11-03 22:12:33 +0000

    Reworked loading of config from on-chip ROM

--- sw/drivers/minotaur.c
+++ sw/drivers/minotaur.c
@@ -2,6 +2,7 @@
 #include <adxdma.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ADXCALL(x) check_status(x, __LINE__)
 
@@ -24,7 +25,7 @@
 #define DATA_SIZE_PER_CORE_MB 512
 #define BANK_SIZE_MB 8192 
 
-#define CONFIG_ROM_SIZE 1
+#define CONFIG_ROM_SIZE 32
 
 #define DDR_BANK_ONE 0x50000000000
 #define DDR_BANK_TWO 0x60000000000
@@ -38,46 +39,67 @@
 
 static LP_STATUS_CODE populate_configuration();
 static int is_initialised();
-static LP_STATUS_CODE get_configuration_data(char*);
-static uint64_t get_core_data_start_address(int);
-static uint64_t get_bank_starting_offset(int, int);
-static int get_dram_bank(int);
+static LP_STATUS_CODE get_configuration_data(int*);
+static void get_bytes(int, char*);
 static int check_status(ADXDMA_STATUS, int);
 
 ADXDMA_HDMA readDMAEngine = ADXDMA_NULL_HDMA, writeDMAEngine=ADXDMA_NULL_HDMA;
 ADXDMA_HWINDOW hWindow = ADXDMA_NULL_HWINDOW;
 ADXDMA_HDEVICE hDevice = ADXDMA_NULL_HDEVICE;
 
-unsigned int number_cores;
 unsigned int core_active_bits;
 unsigned int gpio_write_bits;
 char initialised=0;
+struct device_configuration device_config;
 
 LP_STATUS_CODE minotaur_initialise() {  
   ADXDMA_DEVICE_INFO deviceInfo;
   
+  device_config.ddr_bank_mapping=NULL;
+  device_config.ddr_base_addr_mapping=NULL;
+  
   // Open the device, get information and check the configuration
   if (!ADXCALL(ADXDMA_Open(0, FALSE, &hDevice))) return LP_ERROR;
-  if (!ADXCALL(ADXDMA_GetDeviceInfo(hDevice, &deviceInfo))) return LP_ERROR;
+  if (!ADXCALL(ADXDMA_GetDeviceInfo(hDevice, &deviceInfo))) {
+    minotaur_finalise();
+    return LP_ERROR;
+  }
   
   if (deviceInfo.HardwareID.Vendor != PCI_VENDOR_ID_ALPHADATA ||
       deviceInfo.HardwareID.Device != PCI_DEVICE_ID_ALPHADATA_GENERIC_XDMA ||
       deviceInfo.HardwareID.SubsystemVendor != PCI_SUBVENDOR_ID_ALPHADATA ||
       deviceInfo.HardwareID.SubsystemDevice != PCI_SUBDEVICE_ID_MINOTAUR) {
     fprintf(stderr, "FPGA PDI is not a minotaur based CPU\n");
+    minotaur_finalise();
     return LP_ERROR;
   }
   
   if (deviceInfo.NumH2C == 0 || deviceInfo.NumC2H == 0) {
     fprintf(stderr, "There must be at-least one DMA engine in each direction (H2C and C2H)\n");
+    minotaur_finalise();
     return LP_ERROR;
   }
   
   // Open DMA engines for reading and writing
-  if (!ADXCALL(ADXDMA_OpenDMAEngine(hDevice, 0, FALSE, FALSE, 0, &readDMAEngine))) return LP_ERROR;
-  if (!ADXCALL(ADXDMA_OpenDMAEngine(hDevice, 0, FALSE, TRUE, 0, &writeDMAEngine))) return LP_ERROR;
+  if (!ADXCALL(ADXDMA_OpenDMAEngine(hDevice, 0, FALSE, FALSE, 0, &readDMAEngine))) {
+    minotaur_finalise();
+    return LP_ERROR;
+  }
   
-  if (!ADXCALL(ADXDMA_OpenWindow(hDevice, 0, FALSE, 0, &hWindow))) return LP_ERROR;
+  if (!ADXCALL(ADXDMA_OpenDMAEngine(hDevice, 0, FALSE, TRUE, 0, &writeDMAEngine))) {
+    minotaur_finalise();
+    return LP_ERROR;
+  }
+  
+  if (populate_configuration() != LP_SUCCESS) {
+    minotaur_finalise();
+    return LP_ERROR;
+  }
+  
+  if (!ADXCALL(ADXDMA_OpenWindow(hDevice, 0, FALSE, device_config.pcie_bar_ctrl_window_index, &hWindow))) {
+    minotaur_finalise();
+    return LP_ERROR;    
+  }
   
   core_active_bits=0x0;
   gpio_write_bits=0x0;
@@ -89,35 +111,54 @@ LP_STATUS_CODE minotaur_initialise() {
     minotaur_finalise();
     return LP_ERROR;
   }
-  if (populate_configuration() != LP_SUCCESS) {
-    minotaur_finalise();
-    return LP_ERROR;
-  }
   
   return LP_SUCCESS;
 }
 
-LP_STATUS_CODE minotaur_get_configuration(struct device_configuration * device_config) {
+LP_STATUS_CODE minotaur_get_configuration(struct device_configuration * in_device_config) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
   
-  device_config->name=(char*) malloc(10);
-  sprintf(device_config->name, "Minotaur");
-  device_config->number_cores=number_cores;
-  device_config->architecture_type=LP_ARCH_TYPE_SHARED_INSTR_ONLY;
-  
+  in_device_config->name=(char*) malloc(10);
+  strcpy(in_device_config->name, device_config.name);
+  in_device_config->number_cores=device_config.number_cores;
+  in_device_config->architecture_type=device_config.architecture_type;
+  in_device_config->number_cores=device_config.number_cores;
+  in_device_config->clock_frequency_mhz=device_config.clock_frequency_mhz;
+  in_device_config->pcie_bar_ctrl_window_index=device_config.pcie_bar_ctrl_window_index;
+  in_device_config->version=device_config.version;
+  in_device_config->revision=device_config.revision;
+  in_device_config->instruction_space_size_mb=device_config.instruction_space_size_mb;
+  in_device_config->per_core_data_space_mb=device_config.per_core_data_space_mb;
+  in_device_config->shared_data_space_kb=device_config.shared_data_space_kb;
+  in_device_config->ddr_bank_mapping=(int*) malloc(sizeof(int) * in_device_config->number_cores);
+  in_device_config->ddr_base_addr_mapping=(uint64_t*) malloc(sizeof(uint64_t) * in_device_config->number_cores);
+  for (int i=0;i<in_device_config->number_cores;i++) {
+    in_device_config->ddr_bank_mapping[i]=device_config.ddr_bank_mapping[i];
+    in_device_config->ddr_base_addr_mapping[i]=device_config.ddr_base_addr_mapping[i];
+  }
   return LP_SUCCESS;
 }
 
 LP_STATUS_CODE minotaur_finalise() {
   if (!is_initialised()) return LP_NOT_INITIALISED;
   initialised=0;
-  if (!ADXCALL(ADXDMA_CloseDMAEngine(readDMAEngine))) return LP_ERROR;
+  
+  if (device_config.ddr_bank_mapping != NULL) {
+    free(device_config.ddr_bank_mapping);
+    device_config.ddr_bank_mapping=NULL;
+  }
+  if (device_config.ddr_base_addr_mapping != NULL) {
+    free(device_config.ddr_base_addr_mapping);
+    device_config.ddr_base_addr_mapping=NULL;
+  }
+  
+  if (readDMAEngine != ADXDMA_NULL_HDMA) if (!ADXCALL(ADXDMA_CloseDMAEngine(readDMAEngine))) return LP_ERROR;
   readDMAEngine=ADXDMA_NULL_HDMA;
-  if (!ADXCALL(ADXDMA_CloseDMAEngine(writeDMAEngine))) return LP_ERROR;
+  if (writeDMAEngine != ADXDMA_NULL_HDMA) if (!ADXCALL(ADXDMA_CloseDMAEngine(writeDMAEngine))) return LP_ERROR;
   writeDMAEngine=ADXDMA_NULL_HDMA;
-  if (!ADXCALL(ADXDMA_CloseWindow(hWindow))) return LP_ERROR;
+  if (hWindow != ADXDMA_NULL_HWINDOW) if (!ADXCALL(ADXDMA_CloseWindow(hWindow))) return LP_ERROR;
   hWindow=ADXDMA_NULL_HWINDOW;
-  if (!ADXCALL(ADXDMA_Close(hDevice))) return LP_ERROR;  
+  if (hDevice != ADXDMA_NULL_HDEVICE) if (!ADXCALL(ADXDMA_Close(hDevice))) return LP_ERROR;  
   hDevice=ADXDMA_NULL_HDEVICE;
   return LP_SUCCESS;
 }
@@ -149,6 +190,7 @@ LP_STATUS_CODE minotaur_read_data(uint64_t address, char* data, uint64_t code_si
 
 LP_STATUS_CODE minotaur_start_core(int core_id) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   int core_bit=1 << core_id;
   if ((core_bit & core_active_bits) == 0) {
     // Core is not active, therefore need to start it
@@ -162,6 +204,7 @@ LP_STATUS_CODE minotaur_start_core(int core_id) {
 
 LP_STATUS_CODE minotaur_stop_core(int core_id) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   int core_bit=1 << core_id;
   if (core_bit & core_active_bits != 0) {
     // Core is active, therefore need to stop it
@@ -176,8 +219,7 @@ LP_STATUS_CODE minotaur_stop_core(int core_id) {
 LP_STATUS_CODE minotaur_start_allcores() {
   if (!is_initialised()) return LP_NOT_INITIALISED;
   // Set all cores to be active by driving areset high
-  core_active_bits=(1 << number_cores) - (1 << 0);
-  printf("0x%x\n", core_active_bits);
+  core_active_bits=(1 << device_config.number_cores) - (1 << 0);  
   if (!ADXCALL(ADXDMA_WriteWindow(hWindow, 0, 4, CORE_RESET_GPIO_OFFSET, sizeof(int), &core_active_bits, NULL))) return LP_ERROR;
   return LP_SUCCESS;
 }
@@ -198,20 +240,23 @@ LP_STATUS_CODE minotaur_write_instructions(uint64_t address, const char * byte_d
 
 LP_STATUS_CODE minotaur_write_core_data(int core_id, uint64_t address, const char * byte_data, uint64_t data_size) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
-  uint64_t start_address=get_core_data_start_address(core_id);
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
+  uint64_t start_address=device_config.ddr_base_addr_mapping[core_id];
   if (!ADXCALL(ADXDMA_WriteDMA(writeDMAEngine, 0, start_address + address, byte_data, data_size, NULL))) return LP_ERROR;
   return LP_SUCCESS;
 }
 
 LP_STATUS_CODE minotaur_read_core_data(int core_id, uint64_t address, char * byte_data, uint64_t data_size) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
-  uint64_t start_address=get_core_data_start_address(core_id);
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
+  uint64_t start_address=device_config.ddr_base_addr_mapping[core_id];
   if (!ADXCALL(ADXDMA_ReadDMA(readDMAEngine, 0,start_address + address, byte_data, data_size, NULL))) return LP_ERROR;
   return LP_SUCCESS;
 }
 
 LP_STATUS_CODE minotaur_read_gpio(int core_id, int location, char * data) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   int gpio_data;
   uint64_t addr = CORE_START_OFFSET + (CORE_CTRL_ADDR_SIZE * core_id) + CORE_GPIO_OFFSET;
   if (!ADXCALL(ADXDMA_ReadWindow(hWindow, 0, 4, addr, sizeof(int), &gpio_data, NULL))) return LP_ERROR;  
@@ -224,6 +269,7 @@ LP_STATUS_CODE minotaur_read_gpio(int core_id, int location, char * data) {
 
 LP_STATUS_CODE minotaur_write_gpio(int core_id, int location, char setting) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   int bit_mask=1 << location;
   if (setting == 0) {
     gpio_write_bits= gpio_write_bits ^ bit_mask;
@@ -240,6 +286,7 @@ LP_STATUS_CODE minotaur_write_gpio(int core_id, int location, char setting) {
 
 LP_STATUS_CODE minotaur_uart_has_data(int core_id, int * has_data) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   char uart_status=0;
   uint64_t addr = CORE_START_OFFSET + (CORE_CTRL_ADDR_SIZE * core_id) + CORE_UART_OFFSET + 0x8;
   if (!ADXCALL(ADXDMA_ReadWindow(hWindow, 0, 1, addr, sizeof(char), &uart_status, NULL))) return LP_ERROR;  
@@ -250,6 +297,7 @@ LP_STATUS_CODE minotaur_uart_has_data(int core_id, int * has_data) {
 
 LP_STATUS_CODE minotaur_read_uart(int core_id, char * data) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   int has_data=0;  
   if (minotaur_uart_has_data(core_id, &has_data) != LP_SUCCESS) return LP_ERROR;
   if (has_data) {
@@ -265,11 +313,13 @@ LP_STATUS_CODE minotaur_read_uart(int core_id, char * data) {
 
 LP_STATUS_CODE minotaur_write_uart(int core_id, char data) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   // TODO
 }
 
 LP_STATUS_CODE minotaur_raise_interrupt(int core_id, int irq) {
   if (!is_initialised()) return LP_NOT_INITIALISED;
+  if (core_id >= device_config.number_cores) return LP_UNKNOWN_CORE;
   // TODO
 }
 
@@ -299,43 +349,48 @@ struct device_drivers setup_minotaur_device_drivers() {
 }
 
 static LP_STATUS_CODE populate_configuration() {
-  char config_info[CONFIG_ROM_SIZE];
+  int config_info[CONFIG_ROM_SIZE];
   if (get_configuration_data(config_info) != LP_SUCCESS) return LP_ERROR;
-  number_cores=(int) config_info[0];
+  char byte_data[4];
+  get_bytes(config_info[0], byte_data);
+  device_config.name=(char*) malloc(10);
+  sprintf(device_config.name, "Minotaur");
+  device_config.architecture_type=LP_ARCH_TYPE_SHARED_INSTR_ONLY;
+  device_config.number_cores=(int) byte_data[1];
+  device_config.clock_frequency_mhz=(int) byte_data[2];
+  device_config.pcie_bar_ctrl_window_index=(int) byte_data[3];
+  device_config.version=(byte_data[0] >> 4) & 0xf;  
+  device_config.revision=(int) (byte_data[0] & 0x0F);
+  device_config.instruction_space_size_mb=config_info[1];
+  device_config.per_core_data_space_mb=config_info[2];
+  device_config.shared_data_space_kb=config_info[3];
+  
+  device_config.ddr_bank_mapping=(int*) malloc(sizeof(int) * device_config.number_cores);
+  device_config.ddr_base_addr_mapping=(uint64_t*) malloc(sizeof(uint64_t) * device_config.number_cores);
+  
+  for (int i=0;i<device_config.number_cores;i++) {
+    device_config.ddr_bank_mapping[i]=((config_info[i+4] >> 24) & 0xFF)-1;
+    // Can get junk in the top 32 bits, hence apply mask to ensure only getting first 32 bits and rest is zero
+    uint64_t addr_offset=0xFFFFFFFF & ((config_info[i+4] & 0x00FFFFFF) << 8);    
+    device_config.ddr_base_addr_mapping[i]=device_config.ddr_bank_mapping[i] == 0 ? addr_offset + DDR_BANK_ONE : addr_offset + DDR_BANK_TWO;
+  }
   return LP_SUCCESS;
+}
+
+static void get_bytes(int data, char * bytes) {
+  bytes[0] = (data >> 24) & 0xFF;
+  bytes[1] = (data >> 16) & 0xFF;
+  bytes[2] = (data >> 8) & 0xFF;
+  bytes[3] = data & 0xFF;  
 }
 
 static int is_initialised() {
   return initialised == 1;
 }
 
-static LP_STATUS_CODE get_configuration_data(char * data) {
+static LP_STATUS_CODE get_configuration_data(int * data) {
   if (!ADXCALL(ADXDMA_ReadDMA(readDMAEngine, 0, CONTROL_BASE_ADDR + CONFIG_ROM_OFFSET, data, CONFIG_ROM_SIZE, NULL))) return LP_ERROR;
   return LP_SUCCESS;
-}
-
-static uint64_t get_core_data_start_address(int core_id) {
-  int ddr_bank=get_dram_bank(core_id);
-  uint64_t start_address=DDR_BANK_ONE ? ddr_bank == 0 : DDR_BANK_TWO;
-  return start_address + get_bank_starting_offset(core_id, ddr_bank);
-}
-
-static uint64_t get_bank_starting_offset(int core_id, int bank) {
-  if (bank == 0) {
-    // If bank 0 then just offset by the instruction memory
-    return (((uint64_t)(BANK_SIZE_MB * 1024) * 1024) * core_id) + (INSTR_SIZE_MB * 1024 * 1024);
-  } else {
-    // If bank 1 then need to figure out what my core ID is relative to the bank
-    int mb_remaining_bank1=BANK_SIZE_MB-INSTR_SIZE_MB;
-    int relative_bank_coreid=core_id - (mb_remaining_bank1 / DATA_SIZE_PER_CORE_MB);
-    return ((uint64_t)(BANK_SIZE_MB * 1024) * 1024) * relative_bank_coreid;
-  }
-}
-
-static int get_dram_bank(int core_id) {
-  int mb_remaining_bank1=BANK_SIZE_MB-INSTR_SIZE_MB;
-  int num_core_bank1=mb_remaining_bank1 / DATA_SIZE_PER_CORE_MB;
-  return 0 ? core_id < num_core_bank1 : 1;
 }
 
 static int check_status(ADXDMA_STATUS status_code, int line_number) {
